9b45b8b169b31c4c2ba9bd48c9dd8accea3a4e17
David Little <david.frank.little@gmail.com>
2021-04-07 10:12:25 -0400

    wip: preview button

--- sanity-studio/dashboardConfig.js
+++ sanity-studio/dashboardConfig.js
@@ -1,8 +1,19 @@
 export default {
     widgets: [
       {
-        name: "gatsby",
-        options: { sites: [{ siteUrl: "https://preview-bcmccatalyst.gtsb.io/" }] },
+        name: "netlify",
+        options: {
+          title: "Website Deployment",
+          sites: [
+            {
+              title: 'Generate A Preview',
+              apiId: "4a637246-0035-4966-9f57-83dd090eb52f",
+              buildHookId: "60674163cf6cd6bb588eacca",
+              name: 'sanity-preview',
+              url: 'https://preview--fervent-ritchie-c19b24.netlify.app/',
+            }
+          ]
+        }
       },
     ],
   };
