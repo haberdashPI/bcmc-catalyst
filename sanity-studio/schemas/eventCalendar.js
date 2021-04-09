6d05e0a7caa9c77c1205c811da81abdb14f08ddc
David Little <david.frank.little@gmail.com>
2021-04-09 15:26:17 -0400

    revert event calendar changes

--- sanity-studio/schemas/eventCalendar.js
+++ sanity-studio/schemas/eventCalendar.js
@@ -1,14 +1,26 @@
 export default {
     name: "eventCalendar",
-    title: "News Letter",
+    title: "Event Calendar",
     type: "object",
-    description: "Display mailchimp messages",
+    description: "Create a list of events from a google clanedar.",
     fields: [
         {
-            title: "Filter",
-            name: "filter",
+            title: "Calendar ID",
+            name: "sendto",
             type: "string",
-            description: "If left blank, include all mailchimp messages ('Campaigns' as mailchimp calls them). You can add text here and only messages that include this text in the subject line will be displayed."
+            description: "In your google calendar, open settings (the gear), click on a calendar, and scroll down, to find this ID (under the heading 'Integrate Calendar'). The calendar needs to be made public for it to be visible on the website."
         },
+        {
+            title: "Default View",
+            name: "default_view",
+            type: "string",
+            options: {
+                list: [
+                    { title: "List", value: "list" },
+                    { title: "Month", value: "month" },
+                    { title: "Week", value: "week" },
+                ]
+            }
+        }
     ]
 }
